a99366244a7af50e836caa6f40fa2a9d14ea8f48
Claire Royer <claireroyer57@gmail.com>
2025-09-09 17:14:46 +0200

    Continue change password page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Home from "./pages/Home";
 import DisconnectedLayout from "./layouts/DisconnectedLayout";
-import ChangePassword from "./pages/ChangePassword";
+import ChangePassword from "./pages/ChangePassword.page.";
 
 const App = () => {
   return (
@@ -10,7 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<DisconnectedLayout />}>
           <Route index element={<Home />} />
-          <Route path="reset-password" element={<ChangePassword />} />
+          <Route path="nouveau-mot-de-passe" element={<ChangePassword />} />
         </Route>
       </Routes>
       <ReactQueryDevtools />
