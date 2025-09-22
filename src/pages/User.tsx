54a2d0e70342eddc3190d438c851d48e0678c3c3
Peter Francois <peter.francois237@hotmail.com>
2025-09-22 17:45:05 +0200

    feat: creat a componant UserLi

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router";
 import PrimaryButton from "../components/utils/PrimaryButton";
 import StatusIsConnect from "../components/user/StatusIsConnect";
+import UserLi from "../components/user/UserLi";
+import { EnvelopeIcon } from "@heroicons/react/24/outline";
 
 // method => PATH
 // path => api/v1/users/:userId
@@ -15,29 +17,49 @@ import StatusIsConnect from "../components/user/StatusIsConnect";
 // Conducteur : peut voir la ligne et le train qui lui sont affectés.
 // Utilisateur connecté voir son profil et peut changer sa photo et son statut
 const User = () => {
+  const user = {
+    name: "Peter",
+    role: "sup",
+    hiredAt: "2025",
+    email: "email",
+    status: true,
+    ligne: ["A"],
+    train: 56,
+  };
   return (
     <div className="my-3  mx-auto">
-      <section className="flex justify-around items-center gap-4 mb-4">
-        <div>
-          <img className="rounded-full" src="https://randomuser.me/api/portraits/women/1.jpg" alt="user lastName" />
+      <section className="flex justify-around items-center gap-8 mb-4">
+        <div  className="flex flex-col gap-3">
+          <img
+            className="rounded-full shadow-xl shadow-neutral-100/15"
+            src="https://randomuser.me/api/portraits/women/1.jpg"
+            alt="user lastName"
+          />
+          {/* input type file pour l'ajout de la nouvelle photo */}
           <Link className="text-sm" to={"/"}>
             Changer la photo
           </Link>
         </div>
         <ul>
-          <li>Nom: </li>
-          <li>Rôle: </li>
-          <li>Embauché depuis le:</li>
+          <UserLi label="Nom" value={user.name} />
+          <UserLi label="Rôle" value={user.role} />
+          <UserLi label="Embauché depuis le" value={user.hiredAt} />
         </ul>
       </section>
       <section>
         <ul>
-          <li>Email: </li>
-          <li>Status: </li>
-          <li>Affectation: </li>
+          <UserLi label="Email" value={user.email} icon={<EnvelopeIcon width={20}></EnvelopeIcon>} />
+          <UserLi label="Status" value={user.status} />
+          <UserLi
+            label="Affectation"
+            value={
+              user.ligne ? `Ligne${user.ligne.length == 1 ? "" : "s"} ${user.ligne.join(", ")}` : `Train ${user.train}`
+            }
+          />
         </ul>
       </section>
       <div className="flex flex-col gap-4 my-4 max-w-52 mx-auto">
+        {/* soit lignes soit trains */}
         <PrimaryButton type="button">Lignes</PrimaryButton>
         <PrimaryButton type="button">Trains</PrimaryButton>
         <PrimaryButton type="submit">Réasignations</PrimaryButton>
