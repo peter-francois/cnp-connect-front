9012f69fb7891b680cd23b729e2362637c58464c
Claire ROYER <claireroyer57@gmail.com>
2025-09-24 13:54:12 +0200

    Edit UserTrain.tsx

--- src/components/user/UserTrain.tsx
+++ src/components/user/UserTrain.tsx
@@ -2,7 +2,7 @@ interface UserTrainInterface {
   train: number | null | undefined;
 }
 const UserTrain = ({ train }: UserTrainInterface) => {
-  return <>{train ? <span className="pl-2">{train}</span> : ""}</>;
+  return <>{train && <span className="pl-2">{train}</span>}</>;
 };
 
 export default UserTrain;
