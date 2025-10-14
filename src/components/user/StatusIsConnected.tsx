9dc9e7a2e2ded4e6750f2afd9b946a40f9805d4b
Peter Francois <peter.francois237@hotmail.com>
2025-10-14 17:24:49 +0200

    update UserDetailsPage
    Co-authored-by: NicolasPalay <NicolasPalay@users.noreply.github.com>
    Co-authored-by: Bipce <Bipce@users.noreply.github.com>

--- src/components/user/StatusIsConnected.tsx
+++ src/components/user/StatusIsConnected.tsx
@@ -2,6 +2,7 @@ interface StatusIsConnectInterface {
   status: boolean;
   customClass: string;
 }
+
 const StatusIsConnected = ({ status, customClass }: StatusIsConnectInterface) => {
   return (
     <div className={`rounded-full mt-1 ml-2 size-3 p-2 ${status ? "bg-green-600" : "bg-red-600"} ${customClass}`}></div>
--- src/pages/user/UserDetailsPage.tsx
+++ src/pages/user/UserDetailsPage.tsx
@@ -27,8 +27,9 @@ const UserDetailsPage = () => {
   const { id } = useParams();
   const [currentUser, setCurrentUser] = useState<UserInterface>();
   const [lines, setLines] = useState<LineInterface[]>([]);
-  const roleUserFromToken: UserRolesEnum = UserRolesEnum.supervisor;
+  const roleUserFromToken: UserRolesEnum = UserRolesEnum.coordinator;
 
+  // @dev use tanstack
   useEffect(() => {
     const getData = async () => {
       const data = await getLines();
@@ -54,16 +55,17 @@ const UserDetailsPage = () => {
             src={currentUser.avatar_url}
             alt={currentUser.firstName}
           />
-          {/* input type file pour l'ajout de la nouvelle photo */}
-          <Link className="text-sm" to={"/"}>
+          {/* @dev input type file pour l'ajout de la nouvelle photo */}
+          <Link className="text-sm text-center" to="/">
             Changer la photo
           </Link>
         </div>
+
         <ul>
           <UserField label="Nom" value={currentUser.firstName} />
-          <UserField label="Rôle" value={currentUser.role} />
+          <UserField label="Rôle" value={currentUser.role.charAt(0).toUpperCase() + currentUser.role.slice(1)} />
           <UserField
-            label="Embauché depuis le"
+            label="Date d'embauche"
             value={new Date(currentUser.hiringAt).toLocaleDateString("fr-FR", {
               day: "2-digit",
               month: "2-digit",
@@ -74,20 +76,22 @@ const UserDetailsPage = () => {
       </section>
 
       <section className="flex justify-between">
-        <ul className=" flex flex-col">
+        <ul className="flex flex-col">
           <div className="flex">
             <UserField label="Email" value={currentUser.email} />
             <Link
               to={`mailto:${currentUser.email}`}
-              className="w-8 h-8 mt-9 ml-5 bg-indigo-600 hover:bg-indigo-900 active:border active:border-indigo-400 text-base py-3 text-indigo-100 rounded-lg cursor-pointer border-box flex center"
+              className="size-8 mt-9 ml-5 bg-indigo-600 hover:bg-indigo-900 active:border active:border-indigo-400 text-base py-3 text-indigo-100 rounded-lg cursor-pointer border-box center"
             >
               <EnvelopeIcon width={20}></EnvelopeIcon>
             </Link>
           </div>
+
           <div className="flex items-center">
             <UserField label="Statut" value={currentUser.isConnected ? "Connecté" : "Non connecté"} />
-            <StatusIsConnected customClass="mt-7 ml-5" status={currentUser.isConnected}></StatusIsConnected>
+            <StatusIsConnected customClass="mt-7 ml-5" status={currentUser.isConnected} />
           </div>
+
           <UserField
             label="Affectation"
             value={
@@ -98,6 +102,7 @@ const UserDetailsPage = () => {
           />
         </ul>
       </section>
+
       <div className="flex flex-col gap-6 my-4 mx-auto">
         {roleUserFromToken != UserRolesEnum.conductor && (
           <Assignment currentUserRole={currentUser.role} lines={lines} />
@@ -107,6 +112,7 @@ const UserDetailsPage = () => {
       </div>
     </div>
   ) : (
+    // @dev page not found
     <span>Aucun utilisateur touvé</span>
   );
 };
