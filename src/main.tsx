08c273c6071c28226874124b517f7ff358443e42
Claire Royer <claireroyer57@gmail.com>
2025-11-21 10:34:59 +0100

    Remove js-cookie lib

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import App from "./App.tsx";
-import "./index.css";
-
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./utils/queryClient.ts";
-
-//const queryClient = new QueryClient()
+import App from "./App.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );
