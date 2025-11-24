adf6601d9439d5dc2a6bec9892d5678867e2a9a8
Claire Royer <claireroyer57@gmail.com>
2025-11-24 16:54:37 +0100

    Add role on links
    Co-authored-by: NicolasPalay <NicolasPalay@users.noreply.github.com>
    Co-authored-by: Peter Francois <Peter-Francois@users.noreply.github.com>

--- src/components/header/ItemLink.tsx
+++ src/components/header/ItemLink.tsx
@@ -1,17 +1,25 @@
 import type { ReactNode } from "react";
 import { NavLink } from "react-router";
+import { UserRolesEnum } from "../../types/enum/UserEnum";
+import type { SafeUserInterface } from "../../types/interfaces/UserInterface";
 
 interface LinkComponentInterface {
   link: string;
+  allowedRolesLink: UserRolesEnum[] | undefined;
+  meUser: SafeUserInterface;
   children: ReactNode;
+  handleOnClick: () => void;
 }
 
-const ItemLink = ({ link, children }: LinkComponentInterface) => {
+const ItemLink = ({ link, children, allowedRolesLink, meUser, handleOnClick }: LinkComponentInterface) => {
   return (
     <NavLink
       to={link}
+      onClick={handleOnClick}
       className={({ isActive }) =>
-        `text-indigo-50 px-4 py-2.5 rounded-2xl hover:text-indigo-500 ${isActive && "text-indigo-500 underline"}`
+        `text-indigo-50 px-4 py-2.5 rounded-2xl hover:text-indigo-500 ${isActive && "text-indigo-500 underline"} ${
+          !allowedRolesLink?.includes(meUser.role) && "hidden"
+        }`
       }
     >
       {children}
