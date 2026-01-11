03e34009d28390977cddc315c4206d4510cbd611
Nicolas Palay <nicolassam33@gmail.com>
2026-01-11 17:10:43 +0100

    add logo layout disconnect

--- src/layouts/DisconnectedLayout.tsx
+++ src/layouts/DisconnectedLayout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router";
 const DisconnectedLayout = () => {
   return (
     <main className="container flex flex-col items-center justify-evenly min-h-screen h-full mx-auto">
+       <div className="px-5"><img src="LOGO-CNP-CONNECT.png" alt="" /></div>
       <Outlet />
     </main>
   );
